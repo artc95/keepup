072abe30e9d2b24070bbc24890f9b593d0fc3699
Arthur Chionh (Hwai Teck) <arthur95chionh@gmail.com>
2020-03-28 17:29:32 +0800

    v1.0 - Initial Commit

---
+++ app/src/main/java/artc/artc/keepup/MainActivity.java
@@ -0,0 +1,14 @@
+package artc.artc.keepup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
