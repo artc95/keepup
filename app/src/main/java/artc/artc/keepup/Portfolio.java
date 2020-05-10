e379e8a899b230246970f91e7b42c18f6954437a
Arthur Chionh (Hwai Teck) <arthur95chionh@gmail.com>
2020-05-10 15:13:27 +0800

    v2.3 - Added functions to Portfolio:
    -- Filter blank to Reset Portfolio
    --- FilterTicker() method will display entire portfolio if Ticker input is blank i.e. ""
    
    -- Calculate and display total Quantity
    --- QtySum() method adds and displays total quantity of shares displayed

--- app/src/main/java/artc/artc/keepup/Portfolio.java
+++ app/src/main/java/artc/artc/keepup/Portfolio.java
@@ -9,15 +9,20 @@ import android.view.ViewGroup;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
+import android.text.TextUtils;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.ValueEventListener;
 
 public class Portfolio extends AppCompatActivity {
 
@@ -30,6 +35,8 @@ public class Portfolio extends AppCompatActivity {
 
     EditText filterTicker;
     Button buttonFilter;
+
+    TextView totalQty;
 
     @Override
     protected void onStart() {
@@ -48,14 +55,14 @@ public class Portfolio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
 
+        databaseShares = FirebaseDatabase.getInstance().getReference();
+        QtySum(databaseShares);
+
         recyclerPortfolio = findViewById(R.id.recyclerPortfolio);
         recyclerPortfolio.setHasFixedSize(true);
         recyclerPortfolio.setLayoutManager(new LinearLayoutManager(this));
 
-        databaseShares = FirebaseDatabase.getInstance().getReference();
-
         options = new FirebaseRecyclerOptions.Builder<Shares>().setQuery(databaseShares, Shares.class).build();
-
         adapter = new FirebaseRecyclerAdapter<Shares, PortfolioViewHolder>(options) {
             @Override
             protected void onBindViewHolder(@NonNull PortfolioViewHolder holder, int position, @NonNull Shares shares) {
@@ -71,11 +78,9 @@ public class Portfolio extends AppCompatActivity {
                 return new PortfolioViewHolder(LayoutInflater.from(Portfolio.this).inflate(R.layout.portfolio_item_layout, viewGroup, false));
             }
         };
-
         recyclerPortfolio.setAdapter(adapter);
 
         filterTicker = findViewById(R.id.filterTicker);
-
         buttonFilter = findViewById(R.id.buttonFilter);
         buttonFilter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,27 +95,75 @@ public class Portfolio extends AppCompatActivity {
     private void FilterTicker() {
         String filter_ticker = filterTicker.getText().toString();
         filterTicker.setText("");
-        databaseSearch = databaseShares.orderByChild("sharesTicker").equalTo(filter_ticker);
 
-        options = new FirebaseRecyclerOptions.Builder<Shares>().setQuery(databaseSearch, Shares.class).build();
+        if(filter_ticker.equals("")){
+            // Filter without filter_ticker input displays entire portfolio
+            databaseShares = FirebaseDatabase.getInstance().getReference();
+            QtySum(databaseShares);
+            options = new FirebaseRecyclerOptions.Builder<Shares>().setQuery(databaseShares, Shares.class).build();
+            adapter = new FirebaseRecyclerAdapter<Shares, PortfolioViewHolder>(options) {
+                @Override
+                protected void onBindViewHolder(@NonNull PortfolioViewHolder holder, int position, @NonNull Shares shares) {
+                    holder.sharesTimestamp.setText(shares.getSharesTimestamp());
+                    holder.sharesTicker.setText(shares.getSharesTicker());
+                    holder.sharesPrice.setText(shares.getSharesPrice());
+                    holder.sharesQty.setText(shares.getSharesQty());
+                }
 
-        adapter = new FirebaseRecyclerAdapter<Shares, PortfolioViewHolder>(options) {
+                @NonNull
+                @Override
+                public PortfolioViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                    return new PortfolioViewHolder(LayoutInflater.from(Portfolio.this).inflate(R.layout.portfolio_item_layout, viewGroup, false));
+                }
+            };
+            recyclerPortfolio.setAdapter(adapter);
+            adapter.startListening();
+        } else {
+            databaseSearch = databaseShares.orderByChild("sharesTicker").equalTo(filter_ticker);
+            QtySum(databaseSearch);
+
+            options = new FirebaseRecyclerOptions.Builder<Shares>().setQuery(databaseSearch, Shares.class).build();
+            adapter = new FirebaseRecyclerAdapter<Shares, PortfolioViewHolder>(options) {
+                @Override
+                protected void onBindViewHolder(@NonNull PortfolioViewHolder holder, int position, @NonNull Shares shares) {
+                    holder.sharesTimestamp.setText(shares.getSharesTimestamp());
+                    holder.sharesTicker.setText(shares.getSharesTicker());
+                    holder.sharesPrice.setText(shares.getSharesPrice());
+                    holder.sharesQty.setText(shares.getSharesQty());
+                }
+
+                @NonNull
+                @Override
+                public PortfolioViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                    return new PortfolioViewHolder(LayoutInflater.from(Portfolio.this).inflate(R.layout.portfolio_item_layout, viewGroup, false));
+                }
+            };
+
+            recyclerPortfolio.setAdapter(adapter);
+            adapter.startListening();
+        }
+    }
+
+    private void QtySum(Query database) {
+        totalQty = findViewById(R.id.totalQty);
+
+        database.addValueEventListener(new ValueEventListener() {
             @Override
-            protected void onBindViewHolder(@NonNull PortfolioViewHolder holder, int position, @NonNull Shares shares) {
-                holder.sharesTimestamp.setText(shares.getSharesTimestamp());
-                holder.sharesTicker.setText(shares.getSharesTicker());
-                holder.sharesPrice.setText(shares.getSharesPrice());
-                holder.sharesQty.setText(shares.getSharesQty());
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int total_qty = 0;
+
+                for (DataSnapshot data:dataSnapshot.getChildren()){
+                    Shares shares = data.getValue(Shares.class);
+                    int qty = Integer.parseInt(shares.getSharesQty());
+                    total_qty = total_qty + qty;
+                }
+
+                totalQty.setText(String.format("%d", total_qty));
             }
 
-            @NonNull
             @Override
-            public PortfolioViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-                return new PortfolioViewHolder(LayoutInflater.from(Portfolio.this).inflate(R.layout.portfolio_item_layout, viewGroup, false));
+            public void onCancelled(@NonNull DatabaseError databaseError) {
             }
-        };
-
-        recyclerPortfolio.setAdapter(adapter);
-        adapter.startListening();
+        });
     }
 }
